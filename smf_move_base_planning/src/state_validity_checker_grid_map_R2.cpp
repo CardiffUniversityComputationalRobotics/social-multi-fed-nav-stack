d396014c0194f817ab3163ff078f2ff4c1331c5e
sasilva1998 <sasilva1998@gmail.com>
2024-03-13 15:59:16 +0000

    feat: reduced index for cc

--- smf_move_base_planning/src/state_validity_checker_grid_map_R2.cpp
+++ smf_move_base_planning/src/state_validity_checker_grid_map_R2.cpp
@@ -92,7 +92,7 @@ bool GridMapStateValidityCheckerR2::isValid(const ob::State *state) const
     {
         const grid_map::Index index(*iterator);
 
-        if (obstacles_grid_map_(index(0), index(1)) > 32)
+        if (obstacles_grid_map_(index(0), index(1)) > 20)
         {
             return false;
         }
@@ -137,7 +137,7 @@ bool GridMapStateValidityCheckerR2::isValidPoint(const ob::State *state) const
 
     if (grid_map_.getIndex(query, index))
     {
-        if (obstacles_grid_map_(index(0), index(1)) > 32)
+        if (obstacles_grid_map_(index(0), index(1)) > 20)
         {
             return false;
         }
