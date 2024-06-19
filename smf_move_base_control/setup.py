864149a389eadfdce67bbd2bbad6749f1dc64512
sasilva1998 <sasilva1998@gmail.com>
2024-06-19 14:24:29 +0100

    chore: ros2 mig

--- smf_move_base_control/setup.py
+++ smf_move_base_control/setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'smf_move_base_control'
+package_name = "smf_move_base_control"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='sasm',
-    maintainer_email='sasilva1998@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="sasm",
+    maintainer_email="sasilva1998@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "base_controller = smf_move_base_control.base_controller:main"
         ],
     },
 )
